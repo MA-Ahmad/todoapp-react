0d08750e419c8b41e970140838f253a5c4ac374e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-02-23 01:46:02 -0800

    updated app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,17 +8,51 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { TodoListItem } from "./TodoListItem"
+  Stack,
+  HStack,
+  StackDivider,
+  Flex
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { TodoListItem } from "./TodoListItem";
+
+type Todo = {
+  text: string;
+  complete: boolean;
+};
+
+const todos: Todo[] = [
+  {
+    text: "Walk the dog",
+    complete: false
+  },
+  {
+    text: "Write app",
+    complete: true
+  }
+];
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <TodoListItem />
+        <Flex justifyContent="center">
+          <VStack
+            divider={<StackDivider borderColor="gray.200" />}
+            spacing={4}
+            align="stretch"
+            width="50vh"
+            padding="5px"
+            border="1px solid"
+            borderColor="gray.200"
+            borderRadius="5px"
+          >
+            <TodoListItem todo={todos[0]} />
+            <TodoListItem todo={todos[1]} />
+          </VStack>
+        </Flex>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
