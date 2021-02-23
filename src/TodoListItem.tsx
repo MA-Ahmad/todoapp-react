e25ee2dae9da0515080e631bd2a52ba97850d0c3
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-02-23 01:45:51 -0800

    updated todolistitem

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,13 +1,5 @@
 import React from "react";
-import {
-  Stack,
-  HStack,
-  VStack,
-  StackDivider,
-  Box,
-  Flex,
-  Checkbox
-} from "@chakra-ui/react";
+import { Checkbox } from "@chakra-ui/react";
 
 interface Todo {
   text?: string;
@@ -18,31 +10,10 @@ interface Props {
   todo: Todo;
 }
 
-export const TodoListItem: React.FC<Props> = ({todo}) => {
+export const TodoListItem: React.FC<Props> = ({ todo }) => {
   return (
-    <Flex justifyContent="center">
-      <VStack
-        divider={<StackDivider borderColor="gray.200" />}
-        spacing={4}
-        align="stretch"
-        width="50vh"
-        padding="5px"
-        border="1px solid"
-        borderColor="gray.200"
-        borderRadius="5px"
-      >
-        {/* <Box h="40px" bg="yellow.200"> */}
-        <Checkbox value="naruto" isChecked={todo.complete}>Naruto</Checkbox>
-        {/* </Box> */}
-        {/* <Box h="40px" bg="tomato"> */}
-        <Checkbox value="naruto">Naruto</Checkbox>
-
-        {/* </Box> */}
-        {/* <Box h="40px" bg="pink.100"> */}
-        <Checkbox value="naruto">Naruto</Checkbox>
-
-        {/* </Box> */}
-      </VStack>
-    </Flex>
+    <Checkbox value="naruto" isChecked={todo.complete}>
+      Naruto
+    </Checkbox>
   );
 };
